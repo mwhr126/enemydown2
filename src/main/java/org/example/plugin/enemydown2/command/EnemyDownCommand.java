8b403bc132195b0cdd33dbe93be809cf3e96375e
miwa <dskipineapple@gmail.com>
2024-11-01 13:58:49 +0900

    中級編DAY23 プレイヤーの周囲の敵を消すのを、出現させた敵に限定する

--- src/main/java/org/example/plugin/enemydown2/command/EnemyDownCommand.java
+++ src/main/java/org/example/plugin/enemydown2/command/EnemyDownCommand.java
@@ -32,7 +32,7 @@ public class EnemyDownCommand extends BaseCommand implements Listener {
   public static final int GAME_TIME = 20;
   private Main main;
   private List<PlayerScore> playerScoreList = new ArrayList<>();
-
+  private List<Entity> spawnEntityList = new ArrayList<>();
 
   /**
    *　コンストラクタ。
@@ -101,6 +101,7 @@ public class EnemyDownCommand extends BaseCommand implements Listener {
     }
 
     playerScore.setGameTime(GAME_TIME);
+    playerScore.setScore(0);
     return playerScore;
   }
 
@@ -140,26 +141,23 @@ public class EnemyDownCommand extends BaseCommand implements Listener {
    * ゲームを実行します。規定の時間内に敵を倒すとスコアが加算されます。合計ストアを時間経過後に表示します。
    *
    * @param player コマンドを実行したプレイヤー
-   * @param nowPlayer プレイヤースコア情報
+   * @param nowPlayerScore プレイヤースコア情報
    */
-  private void gamePlay(Player player, PlayerScore nowPlayer) {
+  private void gamePlay(Player player, PlayerScore nowPlayerScore) {
     Bukkit.getScheduler().runTaskTimer(main, Runnable -> {
-      if(nowPlayer.getGameTime() <= 0) {
+      if(nowPlayerScore.getGameTime() <= 0) {
         Runnable.cancel();
+
         player.sendTitle("ゲームが終了しました",
-            nowPlayer.getPlayerName() + " 合計 " + nowPlayer.getScore() + "点",
+            nowPlayerScore.getPlayerName() + " 合計 " + nowPlayerScore.getScore() + "点",
             0, 60, 0);
-        nowPlayer.setScore(0);
-        List<Entity> nearbyEntities = player.getNearbyEntities(50, 0, 50);
-        for(Entity enemy : nearbyEntities) {
-          switch (enemy.getType()) {
-            case ZOMBIE, SKELETON, WITCH -> enemy.remove();
-          }
-        }
+
+        spawnEntityList.forEach(Entity::remove);
         return;
       }
-      player.getWorld().spawnEntity(getEnemySpawnLocation(player), getEnemy());
-      nowPlayer.setGameTime(nowPlayer.getGameTime() - 5);
+      Entity spawnEntity = player.getWorld().spawnEntity(getEnemySpawnLocation(player), getEnemy());
+      spawnEntityList.add(spawnEntity);
+      nowPlayerScore.setGameTime(nowPlayerScore.getGameTime() - 5);
     }, 0, 5 * 20);
   }
 
