0fc376c646909bb100d243964f013dcc0a148ee0
miwa <dskipineapple@gmail.com>
2024-11-03 00:32:08 +0900

    中級編DAY31 JDBCを使ってたところをMyBatisに変更

--- src/main/java/org/example/plugin/enemydown2/data/ExecutingPlayer.java
+++ src/main/java/org/example/plugin/enemydown2/data/ExecutingPlayer.java
@@ -2,23 +2,22 @@ package org.example.plugin.enemydown2.data;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * EnemyDownのゲームを実行する際のスコア情報を扱うオブジェクト。
+ * EnemyDownのゲームを実行する際のプレイヤー情報を扱うオブジェクト。
  * プレイヤー名、合計点数、日時などの情報を持つ。
  */
 
 @Getter
 @Setter
-public class PlayerScore {
+public class ExecutingPlayer {
 
   private String playerName;
   private int score;
   private int gameTime;
 
-  public PlayerScore(@NotNull String playerName) {
+  public ExecutingPlayer(@NotNull String playerName) {
     this.playerName = playerName;
   }
 }
